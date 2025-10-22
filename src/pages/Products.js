527b3f0a66819f02a04333c61589e0d98f068ef2
Zoryas <gombalesterallen@gmail.com>
2025-10-22 23:03:36 +0800

    navbar added

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div className="page">
+      <h1>Our Products</h1>
+      <p>Browse our collection of amazing products.</p>
+    </div>
+  );
+};
+
+export default Products;
